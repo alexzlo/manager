7759343e48c99ea417ae6677d275c2e855588405
Alexey Zlobin <alexey.zlobin@exposit.com>
2019-06-03 20:04:09 +0300

    Begin Auth Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AngularFontAwesomeModule } from 'angular-font-awesome/dist';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AngularFontAwesomeModule} from 'angular-font-awesome/dist';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AuthModule} from './auth/auth.module';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AngularFontAwesomeModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AngularFontAwesomeModule,
+        FormsModule,
+        AuthModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { AuthComponent } from './auth.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {LoginComponent} from './login/login.component';
+import {RegistrationComponent} from './registration/registration.component';
+import {AuthComponent} from './auth.component';
+import {AppRoutingModule} from '../app-routing.module';
 
 @NgModule({
-  declarations: [LoginComponent, RegistrationComponent, AuthComponent],
+  declarations: [
+    LoginComponent,
+    RegistrationComponent,
+    AuthComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ]
 })
-export class AuthModule { }
+export class AuthModule {
+}
