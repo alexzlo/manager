d0739166aa139d576211d842842b66aa0cd78bf2
Alexey Zlobin <alexey.zlobin@exposit.com>
2019-06-03 18:46:34 +0300

    starter

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to Mnager!');
+    expect(page.getTitleText()).toEqual('Welcome to Manager!');
   });
 
   afterEach(async () => {
